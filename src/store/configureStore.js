f5b51ef016da379766d64feb2c0c56d0a08b1600
Andrianto Lie <andrianto.lie@ii5.com>
2017-03-10 17:33:14 +0800

    move local forage configuration to configurestore

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -11,6 +11,7 @@ import { autoRehydrate, persistStore } from 'redux-persist';
 
 export default function configureStore(initialState) {
 
+  // Configure enhancer
   const enhancer = compose(
     applyMiddleware(
       thunk,
@@ -23,15 +24,28 @@ export default function configureStore(initialState) {
     }) : noop => noop
   );
 
+  // Configure localforage Indexeddb setting ( for redux-persist)
+  localforage.config({
+    driver      : localforage.INDEXEDDB, // Force WebSQL; same as using setDriver()
+    name        : 'bookie',
+    version     : 1.0,
+    // storeName   : 'store_name', // Should be alphanumeric, with underscores.
+    description : 'desc'
+  });
+  localforage.setDriver(localforage.INDEXEDDB);
+
   const store = createStore(
     rootReducer,
     initialState,
     enhancer,
   );
 
-  persistStore(store, {storage: localforage}, () => {
-    console.log('autoRehydrate completed');
-  });
+  // persistStore(store, {
+  //   storage: localforage,
+  //   whitelist: ['setting'] // Only setting wants to be persisted
+  // }, () => {
+  //   console.log('autoRehydrate completed');
+  // });
 
   return store;
 }
