43680904c66765d4e4cc68d17c6c5c8d681fbfd9
Andrianto Lie <andrianto.lie@ii5.com>
2017-03-13 11:29:17 +0800

    use redux to navigate instead of a href

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react'
 import logo from '../../assets/images/login-logo.png';
 import { Form } from 'antd'
 import LoginForm from './Form.jsx'
-export default Form.create()(React.createClass({
+import { NavigateActions } from '../../actions'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.onClickSignup = this.onClickSignup.bind(this);
+  }
+
+  onClickSignup(e) {
+    e.preventDefault();
+    this.props.navigateTo('/signup')
+  }
+
   render() {
     return (
         <div className='ant-layout' id='main-content'>
@@ -12,12 +26,19 @@ export default Form.create()(React.createClass({
                     <img src={ logo } alt=''/>
                     <h2 className='login-welcome'> Welcome to Application </h2>
                     <div className='center-ele'>
-                        <LoginForm></LoginForm>
+                        <LoginForm onClickSignup={ this.onClickSignup } />
                     </div>
                 </div>
             </div>
           </div>
         </div>
     );
-  },
-}));
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    navigateTo: NavigateActions.navigateTo
+  }, dispatch);
+}
+export default connect(null, mapDispatchToProps)(Form.create()(Login))
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React, { Component } from 'react';
 import logo from '../../assets/images/login-logo.png';
-import { Form } from 'antd'
-import SignUpForm from './Form'
-export default Form.create()(React.createClass({
+import { Form } from 'antd';
+import SignUpForm from './Form';
+import { NavigateActions } from '../../actions';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+class Signup extends Component {
+  constructor(props) {
+    super(props);
+    this.onClickLogin = this.onClickLogin.bind(this);
+  }
+
+  onClickLogin(e) {
+    e.preventDefault();
+    this.props.navigateTo('/login')
+  }
+
   render() {
     return (
         <div className='ant-layout' id='main-content'>
@@ -13,12 +27,19 @@ export default Form.create()(React.createClass({
                     <h2 className='margin-tb-25'> Welcome to Application </h2>
                     <p className='font22 margin-btm-20'>Please create your new account</p>
                     <div className='center-ele'>
-                        <SignUpForm></SignUpForm>
+                        <SignUpForm onClickLogin={ this.onClickLogin } />
                     </div>
                 </div>
             </div>
           </div>
         </div>
     );
-  },
-}));
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    navigateTo: NavigateActions.navigateTo
+  }, dispatch);
+}
+export default connect(null, mapDispatchToProps)(Form.create()(Signup))
