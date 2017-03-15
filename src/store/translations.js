8b305348977f3554a43c4766840d1dc1b4512798
--global <--global>
2017-03-15 17:34:22 +0530

    -Added translations -Validations performed on Submit click (removed field level validations)

--- src/store/translations.js
+++ src/store/translations.js
@@ -2,10 +2,28 @@ export const translationsObject = {
   en: {
     application: {
       title: 'Awesome app with i18n!',
-      hello: 'Hello, %{name}!'
+      hello: 'Hello, %{name}!',
+      welcome_title: 'Welcome to Application',
+      loading: 'loading ...',
     },
     date: {
       long: 'MMMM Do, YYYY'
+    },
+    signup: {
+      new_acc_req_text: 'Please create your new account',
+      copy_text: 'Copy',
+      download_rec_text: 'Download Recovery File',
+      acc_name: 'Account Name',
+      password_warning: 'If you lose your password, you will lose all of your funds! Keep your password safe! To download a text file of your password, click the download button below:',
+      cannot_recover_password_warning: 'I understand that Peerplays cannot recover my password.',
+      securely_saved_password_warning: 'I have securely saved my password recovery file.',
+      create_account: 'Create Account',
+      already_account: 'Already have an account?',
+      log_in: 'Log In',
+      premium_acc_text: 'This is a premium name which is not supported by this faucet. Please enter a regular name containing least one dash, a number or no vowels.',
+      password_no_match: 'Password does not match',
+      field_req: 'Field is required',
+      acc_name_taken: 'Account name is already taken'
     },
     export: 'Export %{count} items',
     export_0: 'Nothing to export',
