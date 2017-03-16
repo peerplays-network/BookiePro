03232b8e820d59c9ef7ab1d02414c16a68144981
Nilay Chikani <nilay.chikani@ii5.com>
2017-03-16 09:22:39 +0530

    BugzID - 84 - created html for my account page

--- src/store/translations.js
+++ src/store/translations.js
@@ -4,6 +4,32 @@ export const translationsObject = {
       title: 'Awesome app with i18n!',
       hello: 'Hello, %{name}!'
     },
+    myAccount:{
+      home:'Home',
+      my_account: 'My Account',
+      deposit: 'desposit',
+      deposit_desc: 'Please send your BTC to the address' +
+      ' below',
+      copy: 'copy',
+      withdraw: 'withdraw',
+      withdraw_desc: 'Please select your amount',
+      send: 'send',
+      send_value: 'Your Wallet Address',
+      settings: 'settings',
+      notifications: 'Notifications',
+      time_zone: 'Choose Time Zone',
+      format: 'Choose Format',
+      change_password: 'Change Password',
+      create_Recovery_file: 'Create recovery file',
+      transaction_history: 'Transaction History',
+      period: 'Period',
+      date: 'Date',
+      id: 'ID',
+      time: 'Time',
+      description: 'Description',
+      status: 'Status',
+      amount: 'Amount'
+    },
     date: {
       long: 'MMMM Do, YYYY'
     },
