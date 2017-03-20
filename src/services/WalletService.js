56f0977f6ae7fcd52902d3fad114b1d66dce8ba3
Andrianto Lie <andrianto.lie@ii5.com>
2017-03-20 14:50:05 +0800

    add wallet service to process transaction

--- src/services/WalletService.js
+++ src/services/WalletService.js
@@ -1,37 +1,50 @@
 import _ from 'lodash';
 
 class WalletService {
+
+  /**
+   * Process the given transaction
+   */
   static processTransaction(state, transactionObject) {
+    // Get stored private keys and public keys
     const storedPrivateKeys = state.account.keys;
-    const storedPublicKeys = _.map(storedPrivateKeys, (privateKey) => {
-      return privateKey.toPublicKey().toPublicKeyString()
+    const storedPublicKeys = _.mapValues(storedPrivateKeys, (privateKey) => {
+      return privateKey.toPublicKey().toPublicKeyString();
     });
+
     // Set required fees
     return transactionObject.set_required_fees().then(() => {
       // Get potential signatures
       // Inside, it's trying to ask the blockchain based on the seller account id attached in the transaction
       return transactionObject.get_potential_signatures();
-    }).then(({ potentialPublicKeys }) => {
+    }).then(( result ) => {
+      const potentialPublicKeys = result.pubkeys;
       // Check if none of the potential public keys is equal to our public keys
-      const myPubKeys = _.intersection(potentialPublicKeys, storedPublicKeys);
+      const myPubKeys = _.intersection(potentialPublicKeys, _.values(storedPublicKeys));
       if (_.isEmpty(myPubKeys)) {
         throw new Error('No Potential Signatures');
       }
       // Filter potential signatures to get required keys needed to sign the transaction
       return transactionObject.get_required_signatures(myPubKeys);
     }).then((requiredPublicKeys) => {
+      // Add required keys to the transaction
       _.forEach(requiredPublicKeys, (requiredPublicKey) => {
+        const role = _.findKey(storedPublicKeys, (publicKey) => publicKey === requiredPublicKey);
         // Get private key pair
-        const requiredPrivKey = accountPrivateKeys[requiredPubKey];
+        const requiredPrivateKey = storedPrivateKeys[role];
         // Add signature
-        transactionObject.add_signer(requiredPrivKey, requiredPubKey);
+        transactionObject.add_signer(requiredPrivateKey, requiredPublicKey);
       });
       // Broadcast transaction
-      return transactionObject.broadcast()
+      return transactionObject.broadcast();
     }).then((res) => {
-      console.log('PROCESSING TRANSACTION SUCCESS', res);
+      console.log('Processing Transaction Success', res);
+      // Return res to be caught by the next promise in chain
+      return res;
     }).catch((error) => {
-      console.error('PROCESSING TRANSACTION FAIL', error);
+      console.error('Processing Transaction fails', error);
+      // Throw the error to be caught by next promise in chain
+      throw error;
     });
 
   }
--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,11 @@
 import KeyGeneratorService from './KeyGeneratorService';
 import AccountService from './AccountService';
 import NotificationService from './NotificationService';
+import WalletService from './WalletService';
 
 export {
   KeyGeneratorService,
   AccountService,
-  NotificationService
+  NotificationService,
+  WalletService
 }
