d819384f884b541be0cca95e073f6e69153a1018
Willie Tong <tongsinyin@gmail.com>
2017-03-20 18:02:50 +0800

    basic sport page layout

---
+++ src/components/Sport/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => (
+  <div className='banner'>
+      <div className='sport-name'>
+        AMERICAN FOOTBALL
+      </div>
+  </div>
+)
+
+export default Banner;
---
+++ src/components/Sport/Sport.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Banner from './Banner';
+
+const Sport = () => (
+  <div className='sport-wrapper'>
+    <Banner />
+  </div>
+)
+
+export default Sport;
