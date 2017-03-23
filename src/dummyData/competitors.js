d2c1675cccd8a9f114de9f2b0f52968b005e72c9
harshad pansara <harshad.pansara@ii5.com>
2017-03-23 19:09:12 +0530

    - commit for friday's demo the page works and looks good - dummy data is bound to one particular user which is hardcoded in betactions.js - updated design as per mockup - added dummy data - display currency format which is set in state - event time formatting - updated event type - cancel functionality is pending

--- src/dummyData/competitors.js
+++ src/dummyData/competitors.js
@@ -29,7 +29,76 @@ const competitors = [
     sport_id: '1.100.2',
     name: 'Oklahoma City Thunders'
   },
-
+  {
+    id: '1.102.7',
+    sport_id: '1.100.1',
+    name: 'Carolina Panthers'
+  },
+  {
+    id: '1.102.8',
+    sport_id: '1.100.1',
+    name: 'Denver Broncos'
+  },
+  {
+    id: '1.102.9',
+    sport_id: '1.100.1',
+    name: 'Tampa Bay Buccaneers'
+  },
+  {
+    id: '1.102.10',
+    sport_id: '1.100.1',
+    name: 'Atlanta Falcons'
+  },
+  {
+    id: '1.102.11',
+    sport_id: '1.100.1',
+    name: 'Minnesota Vikings'
+  },
+  {
+    id: '1.102.12',
+    sport_id: '1.100.1',
+    name: 'Tennessee Titans'
+  },
+  {
+    id: '1.102.13',
+    sport_id: '1.100.1',
+    name: 'Cleveland Browns'
+  },
+  {
+    id: '1.102.14',
+    sport_id: '1.100.1',
+    name: 'Philadelphia Eagles'
+  },
+  {
+    id: '1.102.15',
+    sport_id: '1.100.1',
+    name: 'Cincinnati Bengals'
+  },
+  {
+    id: '1.102.16',
+    sport_id: '1.100.1',
+    name: 'New York Jets'
+  },
+  {
+    id: '1.102.17',
+    sport_id: '1.100.1',
+    name: 'Oakland Raiders'
+  },
+  {
+    id: '1.102.18',
+    sport_id: '1.100.1',
+    name: 'New Orleans Saints'
+  },
+  {
+    id: '1.102.19',
+    sport_id: '1.100.1',
+    name: 'San Diego Chargers'
+  },
+  {
+    id: '1.102.20',
+    sport_id: '1.100.1',
+    name: 'Kansas City Chiefs'
+  },
 ];
 
 //TODO: add more of this, please follow sport_id from sports dummy data
