012cad1aa3d377263d582613977f125b234c23c9
jaya.nair <jaya.nair@ii5.com>
2017-03-29 18:25:12 +0530

    -----Change Password Screen-----  - Client side and server side validations done  - Change password and Cancel functionality done  - Redirected to 'My Account' screen on successfull password change  - Display Loading on 'Confirm' button when updation is in progress  - Display success message and hide form when the password has been updated successfully (before redirecting to 'My Account' page

--- src/components/ChangePassword/Form.jsx
+++ src/components/ChangePassword/Form.jsx
@@ -1,28 +1,101 @@
 import React from 'react'
-import { Field, reduxForm } from 'redux-form'
+import { Field, reduxForm } from 'redux-form/immutable'
 let I18n = require('react-redux-i18n').I18n;
 
-const SignUpForm = (props) => {
-  const { handleSubmit } = props
+//Component to render the password field
+const renderPasswordField = ({ placeholder,tabIndex, errors, input, maxLength, type,
+  meta: { touched, error, value} }) => (
+  <div>
+      <input autoComplete='off' placeholder={ placeholder } { ...input } type={ type }
+        tabIndex={ tabIndex } maxLength={ maxLength } />
+      { (touched) && error && <span className='errorText'>{ error }</span> }
+      { tabIndex==='1' && !error && errors && errors.length ? errors.map((err) => { return <span className='errorText' key={ err }>{ err }</span>}) : null }
+  </div>
+);
+
+//Allow to type only characters and digits
+const normalizePassword = (value) => {
+  if (!value) {
+    return value
+  }
+  const onlyNums = value.replace(/[^a-z0-9]/gi,'')
+  if (onlyNums) {
+    return onlyNums
+  }
+}
+
+const ChangePasswordForm = (props) => {
+  const { handleSubmit,onClickCancel,errors,loadingStatus,invalid,asyncValidating,submitting } = props
   return (
     <form onSubmit={ handleSubmit }>
       <div className='form-fields'>
-        <Field name='username' component='input' type='text'/>
+        <Field name='old_password' id='old_password' errors={ errors } maxLength='52'
+          normalize={ normalizePassword }
+          component={ renderPasswordField }  placeholder={ I18n.t('changePassword.current_password') } type='password' tabIndex='1' />
       </div>
       <div className='form-fields'>
-        <Field name='password' component='input' type='password' />
+        <Field name='new_password' id='new_password' errors={ errors } maxLength='52'
+          normalize={ normalizePassword }
+          component={ renderPasswordField } placeholder={ I18n.t('changePassword.new_password') } type='password' tabIndex='2' />
       </div>
       <div className='form-fields'>
-        <Field name='password-confirm' component='input' type='password' placeholder={ I18n.t('changePassword.confirm_password') }/>
+        <Field name='new_password_confirm' id='new_password_confirm' errors={ errors } maxLength='52'
+          normalize={ normalizePassword }
+          component={ renderPasswordField } placeholder={ I18n.t('changePassword.confirm_password') } type='password' tabIndex='3'/>
       </div>
       <div className='form-fields'>
-        <button className='btn cancel-btn grid-100 margin-top-25' type='submit'>{ I18n.t('changePassword.cancel') }</button>
-        <button className='btn btn-regular grid-100 margin-top-25' type='submit' >{ I18n.t('changePassword.confirm') }</button>
+        <button
+          type='button'
+          onClick={ onClickCancel }
+          className={ 'btn ' + (
+            (loadingStatus==='loading' && errors.length===0) ? 'btn-regular-disabled':' cancel-btn') + ' grid-100 margin-top-25' }
+          disabled={ loadingStatus==='loading' && errors.length===0 }>
+          { I18n.t('changePassword.cancel') }
+        </button>
+        <button
+          className={ 'btn ' + (invalid || submitting || asyncValidating ||
+          (loadingStatus==='loading' && errors.length===0) ? 'btn-regular-disabled':' btn-regular') + ' grid-100 margin-top-25' }
+          disabled={ invalid || submitting || asyncValidating ||
+            (loadingStatus==='loading' && errors.length===0) }
+          type='submit' >
+          { loadingStatus==='loading' && errors.length===0  ? I18n.t('application.loading') : I18n.t('changePassword.confirm') }
+        </button>
       </div>
     </form>
   )
 };
 
+//Change Password field validations
+const validateChangePasswordFields = (value,blankFieldErrMsg,minFieldErrMsg) => {
+  const minLength = 22;
+  if (!value || value.trim() === '') {
+    return blankFieldErrMsg;
+  } else if(value.length < minLength){
+    return minFieldErrMsg;
+  }
+}
+
 export default reduxForm({
-  form: 'simple'  // a unique identifier for this form
-})(SignUpForm)
+  form: 'changePasswordForm',  // a unique identifier for this form
+  fields: ['old_password', 'new_password', 'new_password-confirm'],
+  validate: function submit(values) {
+    let errors = {},
+      new_password = values.get('new_password'),
+      new_password_confirm = values.get('new_password_confirm');
+    const minPwdError = I18n.t('changePassword.min_pwd_error');
+    //Old password validations
+    errors.old_password = validateChangePasswordFields(values.get('old_password'),
+                          I18n.t('changePassword.enter_old_password'),minPwdError)
+    //New password validations
+    errors.new_password = validateChangePasswordFields(new_password,
+                          I18n.t('changePassword.enter_new_password'),minPwdError)
+    //Confirm-New password validations
+    errors.new_password_confirm = validateChangePasswordFields(new_password_confirm,
+                          I18n.t('changePassword.confirm_new_password'),minPwdError)
+    //New Password/Confirm password match fields validation
+    if (new_password !== new_password_confirm) {
+      errors.new_password_confirm = I18n.t('signup.password_no_match');
+    }
+    return errors;
+  }
+})(ChangePasswordForm)
