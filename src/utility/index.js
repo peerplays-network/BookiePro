1556326f402e2670ebbe3ed7fa33439512830746
harshad pansara <harshad.pansara@ii5.com>
2017-03-29 11:19:49 +0530

    -unmatched bets fixes -changes as per asset precision 5 -populate and filter the data moved in mapstatetoprops -eventtime format change -localization changes -immutable changes in unmatched bets -used momentjs to format date and placed logic under utility folder -moved mergeobject logic under utility folder

--- src/utility/index.js
+++ src/utility/index.js
@@ -3,11 +3,15 @@ import BlockchainUtils from './BlockchainUtils';
 import ChainTypes from './ChainTypes';
 import StringUtils from './StringUtils';
 import TreeUtils from './TreeUtils';
+import DateUtils from './DateUtils';
+import MergeObjectUtils from './MergeObjectUtils';
 
 export {
   BindToChainState,
   BlockchainUtils,
   ChainTypes,
   StringUtils,
-  TreeUtils
+  TreeUtils,
+  DateUtils,
+  MergeObjectUtils
 };
