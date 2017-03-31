96b6bd67642deac7e666c1fa61baeec78be30ee9
jaya.nair <jaya.nair@ii5.com>
2017-03-31 15:14:51 +0530

    - 'Deposit' screen functionalities:   - Display deposit address (Fake API)   - Generate QR code for the corresponding deposit address   - 'Copy' the deposit address to clipboard   - Navigate to the 'Welcome' screen on clicking on 'Continue'
    - 'Welcome' screen functionality:
      - Navigate to the 'Home' screen on clicking on 'Start Betting now'
    
    - 'Signup' related change:
      - Navigate to the 'Deposit' screen after successfull signin
    
    Following flow implemented:
    Create Account -> Make Deposit -> Welcome -> Home

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -1,10 +1,23 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import { Carousel } from 'antd';
 import CarouselComponent1 from './CarouselComponent1'
 import CarouselComponent2 from './CarouselComponent2'
 import CarouselComponent3 from './CarouselComponent3'
 var I18n = require('react-redux-i18n').I18n;
-class Welcome extends Component{
+import { NavigateActions } from '../../actions';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+class Welcome extends PureComponent{
+  constructor(props){
+    super(props);
+    this.onClickStartBetting = this.onClickStartBetting.bind(this);
+  }
+  //Navigate to the 'Home' screen after clicking on the 'Start Betting Now' button
+  onClickStartBetting(e) {
+    e.preventDefault();
+    this.props.navigateTo('/exchange');
+  }
   render() {
     return (
       <div className='sportsbg'>
@@ -22,8 +35,8 @@ class Welcome extends Component{
               </Carousel>
             </div>
             <div className='registerComponent text-center'>
-              <button className='btn btn-regular'> Start
-                Betting now!
+              <button className='btn btn-regular' onClick={ this.onClickStartBetting }>
+                {I18n.t('welcome.start_betting_now')}
               </button>
             </div>
           </div>
@@ -32,4 +45,9 @@ class Welcome extends Component{
     )
   }
 }
-export default Welcome;
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    navigateTo: NavigateActions.navigateTo
+  }, dispatch);
+}
+export default connect(null, mapDispatchToProps)(Welcome)
