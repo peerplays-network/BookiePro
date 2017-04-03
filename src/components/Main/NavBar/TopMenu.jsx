7f0775ec77f03c57418f75d29821826538dd3653
Ajay Matharu <ajay.matharu@ii5.com>
2017-04-03 12:03:44 +0530

    - removed a tag from the menu item - fixed the navigations - updated the icons (used the closest ones from antd till we have the icons ready) - implemented navigateto action to navigate across pages

--- src/components/Main/NavBar/TopMenu.jsx
+++ src/components/Main/NavBar/TopMenu.jsx
@@ -1,26 +1,14 @@
 import React, { Component } from 'react';
-import { Badge, Menu, Icon,Dropdown} from 'antd';
-import Deposit from '../../MyAccount/Deposit'
-import Withdraw from '../../MyAccount/Withdraw'
-import Amount from './AmountDropDown'
-import Notification from './Notification'
-import DropdownMenu from './DropdownMenu'
-const amountCard = (
-  <Amount cardClass='bookie-amount-card' />
-);
-const depositCard = (
-  <Deposit cardClass='bookie-card deposit-card' />
-);
-const withdrawCard = (
-  <Withdraw cardClass='bookie-card withdraw-card' />
-);
-const dropdownMenuCard = (
-  <DropdownMenu cardClass='menu-card' />
-);
-const notificationCard = (
-  <Notification cardClass='notification-card' />
-);
-console.log(depositCard);
+import { Badge, Menu, Icon, Dropdown } from 'antd';
+import Deposit from '../../MyAccount/Deposit';
+import Withdraw from '../../MyAccount/Withdraw';
+import Amount from './AmountDropDown';
+import Notification from './Notification';
+import DropdownMenu from './DropdownMenu';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+import { NavigateActions } from '../../../actions';
+
 class TopMenu extends Component {
   constructor(props) {
     super(props);
@@ -31,14 +19,47 @@ class TopMenu extends Component {
 
     this.handleClick = this.handleClick.bind(this);
   }
+
   handleClick(e) {
-    console.log('click ', e);
+    //TODO:handle selected menu
     this.setState({
       current: e.key,
     });
+
+    switch (e.key) {
+      case 'mywager':
+        this.props.navigateTo('/my-wager');
+        break;
+      case 'myaccount':
+        this.props.navigateTo('/my-account');
+        break;
+      case 'help':
+        //TODO: add navigation action for help page
+        break;
+      case 'logout':
+        //TODO: add action for logout
+        break;
+      default:
+    }
   }
-  //TODO: change href to actions
+
   render() {
+    const amountCard = (
+      <Amount cardClass='bookie-amount-card' />
+    );
+    const depositCard = (
+      <Deposit cardClass='bookie-card deposit-card' />
+    );
+    const withdrawCard = (
+      <Withdraw cardClass='bookie-card withdraw-card' />
+    );
+    const dropdownMenuCard = (
+      <DropdownMenu cardClass='menu-card' onSubmenuClick={ this.handleClick } />
+    );
+    const notificationCard = (
+      <Notification cardClass='notification-card' />
+    );
+
     return (
       <Menu
         className='top-menu'
@@ -47,50 +68,48 @@ class TopMenu extends Component {
         selectedKeys={ [this.state.current] }
         mode='horizontal'
       >
-        <Menu.Item key='money' className='amount'>
+        <Menu.Item key='balance' className='amount'>
           <Dropdown trigger={ ['click'] } overlay={ amountCard } placement='bottomRight'>
             <a className='ant-dropdown-link' href='#'>
               1.234444
             </a>
           </Dropdown>
         </Menu.Item>
-        <Menu.Item key='smile'>
-          <a href='/#/my-wager'> <Icon type='calendar' /> </a>
+        <Menu.Item key='mywager'>
+           <Icon type='calendar' />
         </Menu.Item>
         <Menu.Item key='deposit'>
           <Dropdown trigger={ ['click'] } overlay={ depositCard } placement='bottomRight'>
-            <a className='ant-dropdown-link' href='#'>
               <Icon type='pay-circle-o' />
-            </a>
           </Dropdown>
-
         </Menu.Item>
-        <Menu.Item key='withdrawn'>
+        <Menu.Item key='withdraw'>
           <Dropdown trigger={ ['click'] } overlay={ withdrawCard } placement='bottomRight'>
-            <a className='ant-dropdown-link' href='#'>
               <Icon type='pay-circle-o' />
-            </a>
           </Dropdown>
         </Menu.Item>
-        <Menu.Item key='star' className='notification'>
+        <Menu.Item key='notifications' className='notification'>
           <Dropdown trigger={ ['click'] } overlay={ notificationCard } placement='bottomRight'>
-            <a className='ant-dropdown-link' href='#'>
               <Badge count={ 5 }>
                 <Icon type='notification' />
               </Badge>
-            </a>
           </Dropdown>
         </Menu.Item>
         <Menu.Item key='drop-down'>
-          <Dropdown trigger={ ['click'] } overlay={ dropdownMenuCard } placement='bottomRight'>
-            <a className='ant-dropdown-link' href='#'>
+          <Dropdown key='drop-down-item' trigger={ ['click'] } overlay={ dropdownMenuCard } placement='bottomRight'>
               <Icon type='menu-fold' />
-            </a>
           </Dropdown>
         </Menu.Item>
       </Menu>
     );
   }
 }
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    navigateTo: NavigateActions.navigateTo,
+  }, dispatch);
+}
 
-export default TopMenu;
+export default connect(null,
+  mapDispatchToProps
+)(TopMenu);
