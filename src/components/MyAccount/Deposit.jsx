2ecf69e6c6d286f09bb4b83a0a67132ac93a05d8
jaya.nair <jaya.nair@ii5.com>
2017-04-03 10:40:26 +0530

    - 'Deposit' implemented in the 'My Accounts' page

--- src/components/MyAccount/Deposit.jsx
+++ src/components/MyAccount/Deposit.jsx
@@ -5,25 +5,30 @@ import {
   Input,
 } from 'antd';
 import QRCode from 'qrcode.react';
-
+import copy from 'copy-to-clipboard'
 
 class Deposit extends Component{
+  //Copy the Deposit Address to clipboard
+  onClickCopy(depAddr,e) {
+    e.preventDefault();
+    copy(depAddr);
+  }
   render(){
     return(
       <Card className={ this.props.cardClass }
             title={ I18n.t('myAccount.deposit') }>
         <p>{ I18n.t('myAccount.deposit_desc') }</p>
         <p className='text-center margin-tb-30'>
-          <QRCode
-            value='http://facebook.github.io/react/'/>
+          <QRCode value={ JSON.stringify(this.props.depositAddress) }/>
         </p>
         <div
           className='registerComponent pos-relative'>
-          <Input
+          <Input readOnly
             className='bookie-input'
-            defaultValue='163WXbtyK3xrGEFhprM9JgzbZSyCKnc3AC'
+            value={ this.props.depositAddress }
           />
           <button
+            onClick={ this.onClickCopy.bind(this,this.props.depositAddress) }
             className='btn btn-primary copy-btn'>
             { I18n.t('myAccount.copy') }
           </button>
