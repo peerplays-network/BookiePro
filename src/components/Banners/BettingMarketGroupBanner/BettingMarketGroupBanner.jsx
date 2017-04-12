0ea126160e6cb0ea38e1fe95b7b83d9086146bd3
Nilay Chikani <nilay.chikani@ii5.com>
2017-04-12 17:09:25 +0530

    Fogbugz case 58 : 1. Complex Betting widget html done 2. Localization

--- src/components/Banners/BettingMarketGroupBanner/BettingMarketGroupBanner.jsx
+++ src/components/Banners/BettingMarketGroupBanner/BettingMarketGroupBanner.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { I18n } from 'react-redux-i18n';
 
 const renderClockColumn = (number, label) => {
   let digits = (number > 9 ? number.toString() : '0' + number.toString());
   return (
     <div className='column'>
-      <div className='digits'>
-        <span className='digit'>{ digits[0] }</span>
-        <span className='digit'>{ digits[1] }</span>
+      <div className='digit'>
+        <span className='top-half'>{ digits[0] }</span>
+        <span className='bottom-half'>{ digits[0] }</span>
+      </div>
+      <div className='digit'>
+        <span className='top-half'>{ digits[1] }</span>
+        <span className='bottom-half'>{ digits[1] }</span>
       </div>
       <div className='label'>{ label }</div>
     </div>
@@ -16,16 +21,16 @@ const renderClockColumn = (number, label) => {
 const BettingMarketGroupBanner = (props) => (
   <div className='betting-market-group-banner'>
     <div className='event'>
-      <div className='time'>Pre-Live! Starts on 10/01/2017 13:00</div>
+      <div className='time'>{ I18n.t('bettingMarketGroup.match_start_on') }</div>
       <div className='name'>{ props.eventName }</div>
     </div>
     <div className='countdown'>
-      <div className='time-remaining'>Time Remaining</div>
+      <div className='time-remaining'>{ I18n.t('bettingMarketGroup.time_remaining') }</div>
       <div className='clock'>
-        { renderClockColumn(1, 'DAYS') }
-        { renderClockColumn(12, 'HOURS') }
-        { renderClockColumn(0, 'MINS') }
-        { renderClockColumn(0, 'SECS') }
+        { renderClockColumn(1, I18n.t('bettingMarketGroup.days')) }
+        { renderClockColumn(12, I18n.t('bettingMarketGroup.hours')) }
+        { renderClockColumn(0, I18n.t('bettingMarketGroup.mins')) }
+        { renderClockColumn(0, I18n.t('bettingMarketGroup.secs')) }
       </div>
     </div>
   </div>
