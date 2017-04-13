e1e8936f5c78851473dd0a6d3027ad581acdcb7a
Nilay Chikani <nilay.chikani@ii5.com>
2017-04-13 11:50:06 +0530

    Removed duplications of rules_dialogue and homepage property from Translations file

--- src/store/translations.js
+++ src/store/translations.js
@@ -218,22 +218,6 @@ export const translationsObject = {
       'matches are not necessarily televised.</p><p>The extent of any such delay may vary, depending on the set-up through ' +
       'which they are receiving pictures or data.</p>'
     },
-    homepage:{
-      bannerText: 'In play of 100 open games'
-    },
-    rules_dialogue:{
-      title: 'Moneyline - Rules',
-      buttonTitle:'Rules',
-      content: '<p>Predict the result of this match including any overtime that may be played. IN THE EVENT OF A TIE ' +
-      'AFTER OVERTIME ALL BETS PLACED ON THIS MARKET WILL BE VOID. At kick off all unmatched bets will be cancelled ' +
-      'and the market turned in-play, whereafter unmatched bets will not be cancelled at any time. It is the' +
-      ' users own responsibility to manage their own' +
-      ' positions. </p><p>Customers should be aware' +
-      ' that: </br> Transmissions described as live by' +
-      ' some broadcasters may actually be delayed and that all in-play ' +
-      'matches are not necessarily televised.</p><p>The extent of any such delay may vary, depending on the set-up through ' +
-      'which they are receiving pictures or data.</p>'
-    },
     AllSports:{
       bannerText: 'In play of 100 open games'
     },
