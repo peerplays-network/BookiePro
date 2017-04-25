9c5904a3b17e48e8943f550dba63aee9ea8900c9
Ajay Matharu <ajay.matharu@ii5.com>
2017-04-25 12:55:05 +0530

    - on boarding deposit related fixes - updated the deposit icon on top as per fogbugz case 216 - updated background color of application as per fogbugz case 228 - updated height and width of QR code as per design and reduced the size or border around it fogbugz case 244 - updated the messages with bold text. Fogbugz case 243 & 242 - updated padding and font-size. Fogbugz case 241 -

--- src/components/Deposit/Deposit.jsx
+++ src/components/Deposit/Deposit.jsx
@@ -1,18 +1,18 @@
-import React, {PureComponent} from 'react';
-import {Icon, Row, Col, Input} from 'antd'
+import React, { PureComponent } from 'react';
+import { Row, Col, Input } from 'antd'
 import QRCode from 'qrcode.react';
-var I18n = require('react-redux-i18n').I18n;
-import { NavigateActions,AccountActions } from '../../actions';
+import { I18n, Translate } from 'react-redux-i18n';
+import { NavigateActions, AccountActions } from '../../actions';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import copy from 'copy-to-clipboard'
 
 class Deposit extends PureComponent {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.onClickContinue = this.onClickContinue.bind(this);
   }
-  componentDidMount(){
+  componentDidMount() {
     //Get the deposit address
     this.props.getDepositAddress();
   }
@@ -22,55 +22,46 @@ class Deposit extends PureComponent {
     this.props.navigateTo('/welcome');
   }
   //Copy the Deposit Address to clipboard
-  onClickCopy(depAddr,e) {
+  onClickCopy(depAddr, e) {
     e.preventDefault();
     copy(depAddr);
   }
   render() {
     return (
-      <div className='sportsbg' id='main-content'>
+      <div className='depositBackground' id='main-content'>
         <div className='depositComponent'>
           <div className='wrapper'>
             <div className='text-center'>
-              <Icon type='edit'/>
-              <h2 className='login-welcome'>  {I18n.t('deposit.title')} </h2>
-              <div
-                className='center-section deposit-content'>
-                <Row type='flex' gutter={ 100 }
-                     className='row-divided'>
-                  <Col className='or' span={ 12 }>
+              <i className='deposit-icon'></i>
+              <h2>
+                { I18n.t('deposit.title') }
+              </h2>
+              <div className='center-section deposit-content'>
+                <Row type='flex' gutter={ 100 } className='row-divided'>
+                  <Col span={ 12 }>
                     <p>
-                      {I18n.t('deposit.left_description')}
+                      { <Translate value = 'deposit.left_description' dangerousHTML /> }
                     </p>
-                    <div
-                      className='registerComponent pos-relative'>
-                      <Input readOnly
-                        className='bookie-input'
-                        value={ this.props.depositAddress }
-                      />
-                     <button
-                        className='btn btn-regular copy-btn'
-                        onClick={ this.onClickCopy.bind(this,this.props.depositAddress) }>
-                        {I18n.t('deposit.copy')}
+                    <div className='depositAddress pos-relative'>
+                      <Input readOnly className='bookie-input' value={ this.props.depositAddress }/>
+                      <button className='btn btn-regular copy-btn' onClick={ this.onClickCopy.bind(this, this.props.depositAddress) }>
+                        { I18n.t('deposit.copy') }
                       </button>
                     </div>
                   </Col>
-                  <div className='vertical-divider'>{I18n.t('deposit.or')}</div>
+                  <div className='vertical-divider'>{ I18n.t('deposit.or') }</div>
                   <Col span={ 12 }>
-                    <p>{I18n.t('deposit.right_description')}
+                    <p>{ <Translate value = 'deposit.right_description' dangerousHTML /> }
                     </p>
                     <div className='text-center'>
-                      <p className='bookie-qr'><QRCode
-                        className='bookie-qr'
-                        value={ JSON.stringify(this.props.depositAddress) }/>
-                      </p>
+                      <div className='bookie-qr'><QRCode className='bookie-qr' size='165' value={ JSON.stringify(this.props.depositAddress) }/>
+                      </div>
                     </div>
                   </Col>
                 </Row>
-                <div
-                  className='text-center registerComponent margin-top-40'>
+                <div className='text-center margin-top-100'>
                   <button onClick={ this.onClickContinue } className='btn btn-regular'>
-                    {I18n.t('deposit.continue')}
+                    { I18n.t('deposit.continue') }
                   </button>
                 </div>
               </div>
@@ -92,7 +83,7 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
   return bindActionCreators({
     getDepositAddress: AccountActions.getDepositAddress,
-    navigateTo: NavigateActions.navigateTo,
+    navigateTo: NavigateActions.navigateTo
   }, dispatch);
 }
 export default connect(mapStateToProps, mapDispatchToProps)(Deposit)
