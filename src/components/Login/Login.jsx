4bef1e76e7518b9e30c252b587dd0ee9ac0f3071
Nilay Chikani <nilay.chikani@ii5.com>
2017-04-27 12:44:20 +0530

    Fogbugz case - 330 - Size of the logo changed in Login Fogbugz case - 329 - Size of the logo changed in Signup

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -30,7 +30,7 @@ class Login extends Component {
         <div className='loginComponent' >
           <div className='wrapper'>
             <div className='text-center'>
-              <img src={ logo } height='150px' width='136px' alt=''/>
+              <img src={ logo } height='148' width='132' alt=''/>
               <div className='center-ele'>
                 <LoginForm onClickSignup={ this.onClickSignup }
                   onSubmit={ this.handleSubmit } errors={ this.props.errors } status={ this.props.status } />
