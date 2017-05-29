7bce8e2423e2a67239f7302ffb3e5a26dae211f7
Willie Tong <tongsinyin@gmail.com>
2017-05-29 18:01:35 +0800

    control active tab key in state

--- src/components/BettingDrawers/MarketDrawer/MarketDrawer.jsx
+++ src/components/BettingDrawers/MarketDrawer/MarketDrawer.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Tabs } from 'antd';
 import { I18n } from 'react-redux-i18n';
 import BetSlip from './BetSlip';
@@ -6,17 +6,32 @@ import PlacedBets from './PlacedBets';
 
 const TabPane = Tabs.TabPane;
 
-const MarketDrawer = (props) => (
-  <div id='market-drawer'>
-    <Tabs defaultActiveKey='1' type='card'>
-      <TabPane tab={ I18n.t('market_drawer.tab1') } key='1'>
-        <BetSlip currencyFormat={ props.currencyFormat }/>
-      </TabPane>
-      <TabPane tab={ I18n.t('market_drawer.tab2') }  key='2'>
-        <PlacedBets currencyFormat={ props.currencyFormat }/>
-      </TabPane>
-    </Tabs>
-  </div>
-)
+class MarketDrawer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeTab: '1' };
+    this.onTabClick = this.onTabClick.bind(this);
+  }
+
+  onTabClick(key) {
+    this.setState({ activeTab: key });
+  }
+
+  render() {
+    return (
+      <div id='market-drawer'>
+        <Tabs activeKey={ this.state.activeTab } type='card' onTabClick={ this.onTabClick }>
+          <TabPane tab={ I18n.t('market_drawer.tab1') } key='1'>
+            <BetSlip currencyFormat={ this.props.currencyFormat }/>
+          </TabPane>
+          <TabPane tab={ I18n.t('market_drawer.tab2') }  key='2'>
+            <PlacedBets currencyFormat={ this.props.currencyFormat }/>
+          </TabPane>
+        </Tabs>
+      </div>
+    )
+  }
+}
+
 
 export default MarketDrawer;
