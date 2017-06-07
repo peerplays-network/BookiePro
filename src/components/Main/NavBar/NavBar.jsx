c1ac057df7a9d18f1b455f39110d84edc4f834b4
ii5_tom_lee <ii5_tom_lee@ii5-tom-lees-MacBook-Pro.local>
2017-06-07 16:51:39 +0800

    fix main btn bug

--- src/components/Main/NavBar/NavBar.jsx
+++ src/components/Main/NavBar/NavBar.jsx
@@ -26,7 +26,7 @@ class NavBar extends React.Component {
   render(){
     return (
       <Header id='betex-header'>
-        <div className='logo' onClick={ this.props.handleNavigateToHome }>
+        <div className='logo' onClick={ this.handleNavigateToHome }>
           <img alt='logo' src={ logo } />
         </div>
         <SearchMenu
