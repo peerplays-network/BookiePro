40999816775cd0e7fe5f5eb06aa0f7f653791661
Andrianto Lie <andrianto.lie@ii5.com>
2017-07-04 17:38:35 +0800

    try various node to connect to when one node fails

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -1,20 +1,20 @@
-export default {
+import _ from 'lodash';
+
+const blockchainUrls = [
+  'wss://595-dev-blockchain.pixelplex.by/ws',
+  'wss://invalid1.pixelplex.by/ws', // INVALID, USED TO PROVE RETRYING WITH OTHER NODE FUNCTION
+  'wss://invalid2.pixelplex.by/ws', // INVALID, USED TO PROVE RETRYING WITH OTHER NODE FUNCTION
+]
+
+const shuffledBlockhainUrls = _.shuffle(blockchainUrls)
+
+const Config = {
   version: '1.0.0', // TODO: this one should be in sync with version in package.json
   faucetUrls: ['https://595-dev-faucet.pixelplex.by'],
-  blockchainUrls: [
-    'wss://595-dev-blockchain.pixelplex.by/ws',
-    'ws://52.221.226.91:8090',
-    'wss://595-dev-blockchain.pixelplex.by/ws',
-    'wss://bit.btsabc.org/ws',
-    'wss://bts.transwiser.com/ws',
-    'wss://bitshares.dacplay.org:8089/ws',
-    'wss://openledger.hk/ws',
-    'wss://secure.freedomledger.com/ws',
-    'wss://testnet.bitshares.eu/ws',
-    'wss://eu.openledger.info/ws',
-    'ws://52.221.226.91:8090'
-  ],
+  blockchainUrls: shuffledBlockhainUrls,
   softwareUpdateReferenceAccountName: 'peerplays1',
   gatewayAccountName: 'gateway1',
   dummyDataAccountId: '1.2.138' // TODO: remove this later, for the meantime we can use this to configure dummy data
 }
+
+export default Config
