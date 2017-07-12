3043b9dc2ff5330e34e3930d7db74d729319071c
jaya.nair <jaya.nair@ii5.com>
2017-07-12 15:23:18 +0530

    Fogbugz - 493: Home page logo : Cursor link removed from UI and also click event deactivated when on the home page

--- src/components/Main/NavBar/NavBar.jsx
+++ src/components/Main/NavBar/NavBar.jsx
@@ -10,8 +10,16 @@ class NavBar extends React.Component {
 
   constructor(props) {
     super(props);
+    this.state={
+      isHomeScreen : false
+    }
     this.handleNavigateToHome = this.handleNavigateToHome.bind(this);
     this.onRouteChangeHandle = this.onRouteChangeHandle.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    this.setState({ isHomeScreen:
+      nextProps.location.pathname.indexOf('/exchange') !== -1 ? true : false });
   }
 
   //Redirect to 'Home' screen when clicked on 'Home' link on the Breadcrumb
@@ -23,12 +31,21 @@ class NavBar extends React.Component {
     this._searchMenu.onRouteChangeHandle();
   }
 
+  renderLogo(){
+    //Hide cursor and deactivate click event of logo when on home page
+    return(
+      <div
+        className={ 'logo' + (!this.state.isHomeScreen ? ' link' : '') }
+        onClick={ !this.state.isHomeScreen ? this.handleNavigateToHome : null }>
+        <img alt='logo' src={ logo } />
+      </div>
+    )
+  }
+
   render(){
     return (
       <Header id='betex-header'>
-        <div className='logo' onClick={ this.handleNavigateToHome }>
-          <img alt='logo' src={ logo } />
-        </div>
+        { this.renderLogo() }
         <SearchMenu
           ref={ (ref) => this._searchMenu = ref }
           { ...this.props }
