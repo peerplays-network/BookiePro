1cf23ed8874acc38273c15833ec745b89d19f901
harshad pansara <harshad.pansara@ii5.com>
2017-07-24 17:22:54 +0530

    FogBugz-721 set default class "ant-menu-item-default" of menuitem if current selected item is not active

--- src/components/Main/NavBar/DropdownMenu.jsx
+++ src/components/Main/NavBar/DropdownMenu.jsx
@@ -10,13 +10,13 @@ const DropdownMenu = (props) => {
   return(
     <Card className={ props.cardClass }>
       <Menu onClick={ props.onSubmenuClick }>
-        <Menu.Item key='myaccount'>
+        <Menu.Item key='myaccount' className={ window.location.hash === '#/my-account' ? '' : 'ant-menu-item-default' }>
           <i className='account-dropdown-icon'></i> { I18n.t('topbar.myaccount') }
         </Menu.Item>
-        <Menu.Item key='help'>
+        <Menu.Item key='help' className={ window.location.hash === '#/help-and-support' ? '' : 'ant-menu-item-default' }>
           <Icon type='question-circle-o' /> { I18n.t('topbar.help') }
         </Menu.Item>
-        <Menu.Item key='logout'>
+        <Menu.Item key='logout' className='ant-menu-item-default'>
           <Icon type='logout' /> { I18n.t('topbar.signout') }
         </Menu.Item>
       </Menu>
