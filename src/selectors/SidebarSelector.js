8ec15317162928611f4c725ebea834a5aad0777a
ii5_tom_lee <ii5_tom_lee@ii5-tom-lees-MacBook-Pro.local>
2017-08-02 15:27:52 +0800

    not displaying node if it has no child node

--- src/selectors/SidebarSelector.js
+++ src/selectors/SidebarSelector.js
@@ -91,16 +91,16 @@ const getSidebarCompleteTree = createSelector(
             });
             // Append betting market group to event node
             eventNode = eventNode.set('children', bettingMktGroupNodes);
-            return eventNode;
-          });
+            return eventNode.get('children').size > 0 ? eventNode : null;
+          }).filter( node => node !== null);;
           // Append event to event group
           eventGroupNode = eventGroupNode.set('children', eventNodes);
-          return eventGroupNode;
-        });
+          return eventGroupNode.get('children').size > 0 ? eventGroupNode : null;
+        }).filter( node => node !== null);;
         // Append event group to sport
         sportNode = sportNode.set('children', eventGroupNodes);
-        return sportNode;
-      });
+        return  sportNode.get('children').size > 0 ? sportNode : null;
+      }).filter( node => node !== null);
       // Append sport to complete tree
       completeTree = completeTree.concat(sportNodes);
     }
