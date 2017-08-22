03769d921ced50b8fc1c279c518e984d0908536f
Willie Tong <tongsinyin@gmail.com>
2017-08-22 16:13:30 +0800

    simple doc for BettingWidgets folder

--- src/components/BettingWidgets/index.js
+++ src/components/BettingWidgets/index.js
@@ -1,3 +1,15 @@
+/**
+ * Betting widgets are used to display the best available odds for moneyline market.
+ *
+ * The SimpleBettingWidget, also known as Quick Market Betting Widget, is used in
+ * AllSports (Home) and EventGroup components. It can display the best available
+ * odds for multiple events (each even appears in its own row).
+ *
+ * The ComplexBettingWidget, also known as Full Market Betting Widget, is used in
+ * the Sports component only. It show the best available odds for one event only
+ * and has a more complex (hence the name) UI design than its "simple" counterpart.
+ */
+
 import ComplexBettingWidget from './ComplexBettingWidget';
 import SimpleBettingWidget from './SimpleBettingWidget';
 export {
