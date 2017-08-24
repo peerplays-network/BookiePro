bce5221aca2afa54bc1ba12e744e60161fa88513
Willie Tong <tongsinyin@gmail.com>
2017-08-24 15:14:49 +0800

    simple intro to betting drawers folder

--- src/components/BettingDrawers/index.js
+++ src/components/BettingDrawers/index.js
@@ -1,3 +1,13 @@
+/**
+ * Betting Drawer is where the users place and manage their bets in the Bookie
+ * application. There are two types of Betting Drawer:
+ *
+ * The Quick Bet Drawer is available for screens where more than one market is
+ * presented in the main area, i.e. the Home (AllSports), Sport and Division (
+ * EventGroup) pages.
+ *
+ * The MArket Drawer is only applicable for the Market (Event) Screen.
+ */
 import QuickBetDrawer from './QuickBetDrawer';
 import MarketDrawer from './MarketDrawer';
 export {
