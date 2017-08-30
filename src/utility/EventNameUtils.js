e73e97ca40d7f8c54d3179f4845f9dc3819d3e36
ii5_tom_lee <ii5_tom_lee@ii5-tom-lees-MacBook-Pro.local>
2017-08-30 18:01:01 +0800

    update doc in eventname util and exportutil

--- src/utility/EventNameUtils.js
+++ src/utility/EventNameUtils.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
-/*
+/**
  * Take an event name and return JSX component that displays the event name
  * in 2 rows with line break right after the word `vs`.
  * The JSX component returned is wrapped in a <span> element.
  *
  * If the event name does not contains `vs`, it will be returned as a JSX component too.
  * But the name will be unchanged.
+ *
+ * @param {string} - name, date to be formated, assumed to be in unixformat date
+ * @returns {JSX component} - component displaying event name
  */
 const breakAtVs = (name) => {
   let formatted = (<span>{ name }</span>);
