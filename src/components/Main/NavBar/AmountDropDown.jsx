30fff0a8c2f5dee86fc542029770e016d6c8fedd
Willie Tong <tongsinyin@gmail.com>
2017-09-06 14:43:12 +0800

    edited the content

--- src/components/Main/NavBar/AmountDropDown.jsx
+++ src/components/Main/NavBar/AmountDropDown.jsx
@@ -1,6 +1,8 @@
 /**
- * AmountDropDown used to display available balance and in-game balance
- * AmountDropDown was used in TopBar but not now it is hidden
+ * AmountDropDown displays the available balance of the account and in-game balance.
+ * In-game balance refers to the amount currently locked in both matched and unmatched bets.
+ *
+ * This widget used to be part of the top menu bar but it is now not in use.
  */
 import React, { PureComponent } from 'react';
 let I18n = require('react-redux-i18n').I18n;
@@ -9,12 +11,10 @@ import {
   Row,
   Col
 } from 'antd';
+
+//TODO Rename this component in order to match the file name (or rename the file if appropriate)
 class Amount extends PureComponent{
 
-  /**
-   * This function renders card column for amount balance display
-   * amount displayed with currency prefix
-   */
   renderAmount(amountTypeText,amount){
     return(
       <Col span={ 12 }>
