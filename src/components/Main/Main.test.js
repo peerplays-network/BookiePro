bfbea86b58bac1db2e319f223ce94a2aaa4496ee
jaya.nair <jaya.nair@ii5.com>
2017-09-11 18:51:16 +0530

    My Account - Deposit component (dumb component) test cases (UI and click event simulation)

--- src/components/Main/Main.test.js
+++ src/components/Main/Main.test.js
@@ -3,10 +3,8 @@ import renderer from 'react-test-renderer';
 import { Router, Route, createMemoryHistory } from 'react-router';
 import { mount } from 'enzyme';
 import Main from './Main';
-import SideBar from './../SideBar';
-import NavBar from './../Main/NavBar';
-import { Provider } from 'react-redux';
-import configureStore from './../../store/configureStore';
+import SideBar from './SideBar';
+import NavBar from './NavBar';
 
 jest.mock('peerplaysjs-lib');
 
@@ -17,18 +15,12 @@ describe('The main app', () => {
   beforeEach(() => {
     let history = createMemoryHistory()
     let location = history.createLocation('/')
-    const store = configureStore();
-
     let routes = (
-      <Provider store={ store }>
-        <Router history={ history } location={ location }>
-          <Route path='/' component={ Main } />
-        </Router>
-      </Provider>
+      <Router history={ history } location={ location }>
+        <Route path='/' component={ Main } />
+      </Router>
     )
     testApp = mount(routes, { attachTo: document.getElementById('root') });
-    console.warn( ' testApp is', testApp)
-
   })
 
   // TODO: Not sure how to get snapshot to work. Skipping it for now.
@@ -37,16 +29,10 @@ describe('The main app', () => {
     expect(tree).toMatchSnapshot();
   });
 
-  // it('should render NavBar and SideBar', () => {
-  //   console.warn( ' testApp dd', testApp)
-  //
-  //   // Cannot use #containsAllMatchingElements because
-  //   // SideBar is embedded in a <Layout /> component
-  //   // expect(testApp.containsMatchingElement(<SideBar />)).toBe(true);
-  //   expect(testApp.containsMatchingElement(<NavBar />)).toBe(true);
-  // } );
-  //
-  // it('should render a message if items are empty', () => {
-  //
-  // })
+  it('should render NavBar and SideBar', () => {
+    // Cannot use #containsAllMatchingElements because
+    // SideBar is embedded in a <Layout /> component
+    expect(testApp.containsMatchingElement(<SideBar />)).toBe(true);
+    expect(testApp.containsMatchingElement(<NavBar />)).toBe(true);
+  } );
 });
--- src/components/MyAccount/Deposit.test.js
+++ src/components/MyAccount/Deposit.test.js
@@ -2,24 +2,63 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { Deposit } from './Deposit'
 
+function setup() {
+  const props = {
+    depositAddress: 'aaa',
+    cardClass: 'bookie-card depositCardComponent'
+  }
+  const depositWrapper = shallow(<Deposit { ...props } />)
+  return {
+    props,
+    depositWrapper
+  }
+}
+
 describe('Test cases on "Deposit" component',()=>{
 
-  let depositWrapper
-  //need to get this dynamically
-  const depositAddress = '2423432432';
-
-  const cardClass = 'bookie-card depositCardComponent'
-
-  beforeEach(()=>{
-    depositWrapper = shallow(<Deposit depositAddress={ depositAddress } cardClass={ cardClass }/>)
-  })
+  const { depositWrapper } = setup()
 
   it('Check rendering of the "Deposit" component', () => {
     expect(depositWrapper.length).toEqual(1)
   });
 
-  it('Check deposit address validity', () => {
-    expect(depositWrapper.find('Input').prop('value')).toEqual('43534gfd')
+  it('Check if "Deposit" component has class name', () => {
+    expect(depositWrapper.is('.bookie-card depositCardComponent')).toBe(true)
+  });
+
+  it('Check rendering of the qrcode "QRCode" component in the "Deposit" component', () => {
+    expect(depositWrapper.find('QRCode').length).toEqual(1)
+  });
+
+  it('Check rendering of the antd "Input" component in the "Deposit" component', () => {
+    expect(depositWrapper.find('Input').length).toEqual(1)
+  });
+
+  it('Check rendering of html button in the "Deposit" component', () => {
+    expect(depositWrapper.find('button').length).toEqual(1)
+  });
+
+  it('Check click event of Copy button', () => {
+
+    /*
+    Click event is simulated successfully here
+    However 'copy' function of the 'copy-to-clipboard' module throws the following js error
+    'reselectPrevious is not a function'
+    Error reason reference link: https://github.com/nkbt/react-copy-to-clipboard/issues/20
+    It says:
+    'You are trying to call 'copy-to-clipboard' when there's no interaction by user (clicks/keydowns, etc).
+    Copy commands triggered from document.execCommand() will only affect the contents of the real clipboard
+    if the event is dispatched from an event that is trusted and triggered by the user,
+    or if the implementation is configured to allow this'
+    */
+
+    depositWrapper.find('button').simulate('click',{ preventDefault() {} },'aaa')
+    /*
+    OR another way of doing it. Use jest 'mock'
+    const mockCallback = jest.fn();
+    copy('bbb', mockCallback);
+    expect(mockCallback.mock.calls.length).toBe(1);
+    */
   });
 
 });
