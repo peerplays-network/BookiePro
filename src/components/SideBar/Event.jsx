3f9459d475cd95c2171768183b082208fc47a74b
Unknown <k.francis@pbsa.info>
2017-09-18 14:49:34 -0300

    BOOK-1
    - Did Some work to implement the dates showing up on the sidebar

--- src/components/SideBar/Event.jsx
+++ src/components/SideBar/Event.jsx
@@ -1,18 +1,23 @@
 import React, { PureComponent } from 'react';
-import { EventNameUtils } from '../../utility';
+import { EventNameUtils, DateUtils } from '../../utility';
 import './Event.less';
 
 class Event extends PureComponent {
   render() {
     const { id, onClick, data, name } = this.props;
+
+    let date = DateUtils.getMonthAndDay(data.start_date);
     return (
-      <div className='event-node-container' key={ id } onClick={ onClick  }>
-        <div className={ `event-label-container${data.isSelected ? '-selected' : ''}` }>
-          <label>{ EventNameUtils.breakAtVs(name) }</label>
-          {
-            data.isLiveMarket &&
-            <span className='badge' />
-          }
+      <div>
+        <label className='event-date-header'>{ date }</label>
+        <div className='event-node-container' key={ id } onClick={ onClick  }>
+          <div className={ `event-label-container${data.isSelected ? '-selected' : ''}` }>
+            <label>{ EventNameUtils.breakAtVs(name) }</label>
+            {
+              data.isLiveMarket &&
+              <span className='badge' />
+            }
+          </div>
         </div>
       </div>
     );
