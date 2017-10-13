695379c13accbaf0e1edf31c7e34abaa143ff65d
Keegan Francis <k.francis@pbsa.info>
2017-10-13 15:13:22 -0300

    BOOK-139  - Centered the powered by peerplays logo

--- src/components/PeerPlaysLogo/PeerPlaysLogo.jsx
+++ src/components/PeerPlaysLogo/PeerPlaysLogo.jsx
@@ -13,11 +13,12 @@
 import React, { PureComponent } from 'react';
 import { I18n } from 'react-redux-i18n';
 import PoweredByPPLogoPath from '../../assets/images/powerbypp.png';
+import "./PeerPlaysLogo.less"
 
 class PeerPlaysLogo extends PureComponent {
   render() {
     return (
-      <img alt={ I18n.t('application.peerplays_logo_title') } src={ PoweredByPPLogoPath }
+      <img id='poweredByPPLogo' alt={ I18n.t('application.peerplays_logo_title') } src={ PoweredByPPLogoPath }
         width={ 130 } height={ 22 }></img>
     )
   }
