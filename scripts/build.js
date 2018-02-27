d7eb7318dd797223c46b154c3e9a71d2005a1e67
Andrew Moore <a.moore@pbsa.info>
2018-02-27 13:26:14 -0400

    BOOK-502 : Cleaning up the Bookie build process to exclude the electron compilation pieces

--- scripts/build.js
+++ scripts/build.js
@@ -134,9 +134,9 @@ function build(previousSizeMap) {
     }
 
     if (process.env.CI && stats.compilation.warnings.length) {
-     printErrors('Failed to compile. When process.env.CI = true, warnings are treated as failures. Most CI servers set this automatically.', stats.compilation.warnings);
-     process.exit(1);
-   }
+      printErrors('Failed to compile. When process.env.CI = true, warnings are treated as failures. Most CI servers set this automatically.', stats.compilation.warnings);
+      process.exit(1);
+    }
 
     console.log(chalk.green('Compiled successfully.'));
     console.log();
