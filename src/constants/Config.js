d41d434e75a43ebc202d440c6759d366ebd32ea7
Keegan Francis <k.francis@pbsa.info>
2018-05-07 23:24:08 +0800

    BOOK-302 - Replaced Double Quotes with Single Quotes

--- src/constants/Config.js
+++ src/constants/Config.js
@@ -6,7 +6,7 @@ import { version } from '../../package.json'
 const blockchainUrls = [
   // 'wss://595-dev-blockchain.pixelplex.by/ws',
   //'wss://peerplays-dev.blocktrades.info/',  
-  "ws://ec2-35-183-1-21.ca-central-1.compute.amazonaws.com:8090"
+  'ws://ec2-35-183-1-21.ca-central-1.compute.amazonaws.com:8090'
 // 'wss://api.ppytest.blckchnd.com'
 ]
 
