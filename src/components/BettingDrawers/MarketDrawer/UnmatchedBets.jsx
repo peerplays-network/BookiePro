02c102ee1e02f0f188e72bac1aa370410b4edc77
mseaward <m.seaward@pbsa.info>
2018-06-25 09:46:16 -0300

    - added configuration values to core config file 	- exchange coin dust: precision of three covered 	- coin dust: precision of 5 covered 	- mili coin dust: precision of 2 covered - code cleaning

--- src/components/BettingDrawers/MarketDrawer/UnmatchedBets.jsx
+++ src/components/BettingDrawers/MarketDrawer/UnmatchedBets.jsx
@@ -15,11 +15,11 @@ import { bindActionCreators } from 'redux';
 import { Button } from 'antd';
 import Immutable from 'immutable';
 import { I18n } from 'react-redux-i18n';
-import { BettingModuleUtils, CurrencyUtils } from '../../../utility';
+import { CurrencyUtils } from '../../../utility';
 import { MarketDrawerActions } from '../../../actions';
 import BetTable from '../BetTable';
 import './UnmatchedBets.less';
-import { BettingDrawerStates, BetTypes } from '../../../constants'
+import { BettingDrawerStates } from '../../../constants'
 import { MyAccountPageSelector } from '../../../selectors'
 
 class UnmatchedBets extends PureComponent {
@@ -61,22 +61,10 @@ const mapStateToProps = (state, props) => {
   const unmatchedBets = state.getIn(['marketDrawer', 'unmatchedBets']);
   // Transform the raw bet data into a specific format for the EditableBetTable
   const originalBets = unmatchedBets;
-  // const oddsFormat = MyAccountPageSelector.oddsFormatSelector(state);
-  // const currencyFormat = MyAccountPageSelector.currencyFormatSelector(state);
   // This is essentially the same procedure used in BetSlip
   let page = Immutable.Map();
   originalBets.forEach((bet) => {
     const betType = bet.get('bet_type');
-
-    // let profit = BettingModuleUtils.getProfitOrLiability(bet.get('stake'), bet.get('odds'), currencyFormat, betType === BetTypes.BACK ? 'profit' : 'liability');
-    // let odds = BettingModuleUtils.oddsFormatFilter(bet.get('odds'), oddsFormat, 'decimal');
-    // let stake = bet.get('stake');
-
-    // profit = CurrencyUtils.isDust(currencyFormat, profit);
-    // bet = bet.set('profit', profit).set('liability', profit).set('odds', odds);
-
-    // stake = CurrencyUtils.isDust(currencyFormat, stake);
-    // bet =  bet.set('stake', stake);
 
     // Page content are grouped by market type (back or lay)
     if (!page.has(betType)) {
