86b83b1e008d7e861d5895dfa7d357d707b45878
Keegan Francis <k.francis@pbsa.info>
2018-07-09 11:44:23 -0300

    BOOK-656

--- src/components/Banners/SportBanner/SportBanner.jsx
+++ src/components/Banners/SportBanner/SportBanner.jsx
@@ -2,19 +2,44 @@ import React, { PureComponent } from 'react';
 import banner1 from '../../../assets/images/sports_banner_1.png';
 import banner2 from '../../../assets/images/sports_banner_2.png';
 import banner3 from '../../../assets/images/sports_banner_3.png';
+
+import baseball from '../../../assets/images/sports_banner_baseball.png';
+import basketball from '../../../assets/images/sports_banner_basketball.png';
+import defaultBanner from '../../../assets/images/sports_banner_default.png';
+import football from '../../../assets/images/sports_banner_football.png';
+import icehockey from '../../../assets/images/sports_banner_hockey.png';
+import soccer from '../../../assets/images/sports_banner_soccer.png';
+
 import PropTypes from 'prop-types';
 
 const bannerUrls = [banner1, banner2, banner3];
-const generateBannerUrl = () => {
-  const indexOfBannerToBeUsed = Math.floor((Math.random() * bannerUrls.length));
-  return bannerUrls[indexOfBannerToBeUsed];
+const sportBannerUrls = {
+  baseball, basketball, defaultBanner, football, soccer, icehockey
+};
+
+const generateBannerUrl = (sport) => {
+
+  // Make the passed in sport lowercase without spaces. The url name is written to conform and match these rules. 
+  const theSport = sport.toLowerCase().replace(/ /g,'');
+
+  // Choose the sport that was passed into the component
+  if (sportBannerUrls[theSport]) {
+    return sportBannerUrls[theSport];
+  } else {
+    // Otherwise, choose a random image from the list of provided images
+    const indexOfBannerToBeUsed = Math.floor((Math.random() * bannerUrls.length));
+    return bannerUrls[indexOfBannerToBeUsed];
+  }
+  
 }
 
 class SportsBanner extends PureComponent {
   constructor(props) {
     super(props);
+    console.log('Sports Banner')
+    console.log(props)
     this.state = {
-      bannerUrl: generateBannerUrl()
+      bannerUrl: generateBannerUrl(props.sport)
     }
   }
 
