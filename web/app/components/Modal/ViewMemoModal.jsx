38e86e76c717fe417cfe51a684c5595c9a83233a
Ian Marquette <i.marquette@pbsa.info>
2018-07-25 14:38:35 -0300

    Added styling for modal memo

--- web/app/components/Modal/ViewMemoModal.jsx
+++ web/app/components/Modal/ViewMemoModal.jsx
@@ -29,22 +29,22 @@ class ViewMemoModal extends React.Component {
                                     <div className="modalTitle"><Translate content="modal.memo.title" /></div>
                                     <div className="modalContent row">
                                         <div className='col-6 modalSender'>
-                                            <p className='modalLabel'><Translate content='dashboard.recent_activity.sender' />:</p>
-                                            <p className='modalValue'>{ this.props.memo.sender }</p>
+                                            <div className='modalLabel'><Translate content='dashboard.recent_activity.sender' />:
+                                            <span className='modalValue'>{ this.props.memo.sender }</span></div>
                                         </div>
                                         <div className='col-6 modalReceiver'>
-                                            <p className='modalLabel'><Translate content='dashboard.recent_activity.receiver' />:</p>
-                                            <p className='modalValue'>{ this.props.memo.receiver }</p>
+                                            <div className='modalLabel'><Translate content='dashboard.recent_activity.receiver' />:
+                                            <span className='modalValue'>{ this.props.memo.receiver }</span></div>
                                         </div>
                                         <div className="col-12 modalMessage">
                                             <p>{ this.props.memo.message }</p>
                                         </div>
-                                        <div className="col-12 modalDate">
-                                            <p><TimeAgo time={new Date(this.props.memo.time)}/></p>
+                                        <div className="col-6 modalDate">
+                                            <div><TimeAgo time={new Date(this.props.memo.time)}/></div>
                                         </div>
-                                    </div>
-                                    <div className="modalFooter text_r">
-                                        <button type="button" className="btn btn-neutral" onClick={this.onClose.bind(this)}><Translate content="done" /></button>
+                                        <div className="col-6 modalConfirm text_r">
+                                            <button type="button" className="btn btn-neutral" onClick={this.onClose.bind(this)}><Translate content="done" /></button>
+                                        </div>    
                                     </div>
                                 </div>
                            </div>
