42834bd81a5e029914a35f668f7388d61047dffb
mseaward <m.seaward@pbsa.info>
2018-08-28 16:09:50 -0300

    Key File

---
+++ src/constants/UserIssuedAssets.js
@@ -0,0 +1,28 @@
+import bitFunBlack from '../assets/icons/bitfun_icon_black.svg';
+import bitFunWhite from '../assets/icons/bitfun_icon_white.svg';
+import mBitFunWhite from '../assets/icons/mbitfun_icon_white.svg';
+import mBitFunBlack from '../assets/icons/mbitfun_icon_black.svg';
+
+import bitCoinBlack from '../assets/icons/bitcoin_icon_black.png';
+import bitCoinWhite from '../assets/icons/bitcoin_icon_white.png';
+import mBitCoinBlack from '../assets/icons/mbitcoin_icon_black.png';
+import mBitCoinWhite from '../assets/icons/mbitcoin_icon_white.png';
+
+
+const UserIssuedAssets = {
+  btf: {
+    black: bitFunBlack,
+    white: bitFunWhite,
+    mBlack: mBitFunBlack,
+    mWhite: mBitFunWhite
+  },
+  btc: {
+    black: bitCoinBlack,
+    white: bitCoinWhite,
+    mBlack: mBitCoinBlack,
+    mWhite: mBitCoinWhite
+  },
+  thing: 'thing'
+};
+
+export default UserIssuedAssets;
