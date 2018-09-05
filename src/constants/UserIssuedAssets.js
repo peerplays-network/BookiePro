0f2bfb779a47fb858a4075531198498a74d5729d
mseaward <m.seaward@pbsa.info>
2018-09-05 16:03:44 -0300

    Update styling again on .btc images.

--- src/constants/UserIssuedAssets.js
+++ src/constants/UserIssuedAssets.js
@@ -3,8 +3,8 @@ import bitFunWhite from '../assets/icons/bitfun_icon_white.svg';
 import mBitFunWhite from '../assets/icons/mbitfun_icon_white.svg';
 import mBitFunBlack from '../assets/icons/mbitfun_icon_black.svg';
 
-import bitCoinBlack from '../assets/icons/bitcoin_icon_black.svg';
-import bitCoinWhite from '../assets/icons/bitcoin_icon_white.svg';
+import bitCoinBlack from '../assets/icons/bitcoin_icon_black.png';
+import bitCoinWhite from '../assets/icons/bitcoin_icon_white.png';
 import mBitCoinBlack from '../assets/icons/mbitcoin_icon_black.png';
 import mBitCoinWhite from '../assets/icons/mbitcoin_icon_white.png';
 
