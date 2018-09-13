c846ac3c58ebd613ebc6b456b279e5b3b3b9d82d
mseaward <m.seaward@pbsa.info>
2018-09-13 14:03:14 -0300

    Add testing code for the new common messaging module to the login component.

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -19,6 +19,8 @@ import {I18n} from 'react-redux-i18n';
 import {AccountService} from '../../services';
 import {AppBackgroundTypes} from '../../constants';
 import FloatingHelp from '../FloatingHelp';
+import CommonMessage from '../CommonMessage/CommonMessage';
+import MessageType from '../../constants/MessageTypes';
 
 class Login extends PureComponent {
   constructor(props) {
@@ -88,6 +90,10 @@ class Login extends PureComponent {
   render() {
     return (
       <div className='loginBackground'>
+        <CommonMessage
+          message={ this.props.message }
+          type={ MessageType.DEFAULT }
+        />
         <div className='loginComponent'>
           <div className='wrapper'>
             <div className='text-center'>
@@ -111,7 +117,8 @@ class Login extends PureComponent {
 
 const mapStateToProps = (state) => ({
   errors: state.getIn(['auth', 'loginErrors']),
-  status: state.getIn(['auth', 'loginLoadingStatus'])
+  status: state.getIn(['auth', 'loginLoadingStatus']),
+  message: 'This is a message'
 });
 
 const mapDispatchToProps = (dispatch) => bindActionCreators(
