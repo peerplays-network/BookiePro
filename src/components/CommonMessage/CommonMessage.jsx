f7cff25e4af9e50ae7f739768354f752cf46b123
mseaward <m.seaward@pbsa.info>
2018-09-21 13:54:29 -0300

    Modified CommonMessage component to use state loading of messages.

--- src/components/CommonMessage/CommonMessage.jsx
+++ src/components/CommonMessage/CommonMessage.jsx
@@ -2,45 +2,57 @@ import React, {PureComponent} from 'react';
 import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import './CommonMessage.less';
-import MessageType from '../../constants/MessageTypes';
 import CommonMessageActions from '../../actions/CommonMessageActions';
+
+const compileMessage = (props) => {
+  // Compile and render the exchange messages.
+  const remove = (e) => {
+    const msgParent = e.target.parentElement.parentElement.parentElement;
+    // remove the message
+    //msgParent.remove();
+    //extract the id to pass to reducer so that we can remove the message from state.
+    const id = msgParent.id;
+    
+    props.hideMessage(id);
+  };
+
+  const eM = props.exchangeMessages.map((pair, key) => (
+    <div className={ 'message-background ' + pair.get('messageType') } key={ key } id={ 'e'+key }>
+      <div className='message-content'>
+        <span>{pair.get('content') } <p onClick={ remove }>X</p></span>
+      </div>
+    </div>
+  ));
+  return <div>{eM}</div>;
+};
 
 class CommonMessage extends PureComponent {
   constructor(props) {
     super(props);
 
     this.state = {isHidden: false};
-    this.isHidden = this.isHidden.bind(this);
-  }
-
-  isHidden(e) {
-    debugger;
-    // Get the id of the message component to hide.
-    const id = e.target.parentElement.parentElement.parentElement.id;
-    this.setState({isHidden: !this.state.isHidden});
-    this.props.hideMessage(id);
   }
 
   render() {
-    let msgClass = 'message-background ';
-
-    if (!this.state.isHidden) {
-      msgClass += this.props.type;
-    } else {
-      msgClass += MessageType.NONE;
-    }
-
-    return(
+    return (
       <div>
-        <div className={ msgClass } id={ this.props.id }>
-          <div className='message-content'>
-            <span>{this.props.message} <p onClick={ this.isHidden }>X</p></span>
-          </div>
-        </div>
-      </div >
+        {compileMessage(this.props)}
+      </div>
     );
   }
 }
+
+const mapStateToProps = (state, ownProps) => {
+  const messageO = ownProps.message;
+  const messages = state.get('commonMessage');
+  const exchangeMessages = messages.get('exchangeMessages');
+  const betslipMessages = messages.get('betslipMessages');
+  return {
+    messageO,
+    exchangeMessages,
+    betslipMessages
+  };
+};
 
 const mapDispatchToProps = (dispatch) => bindActionCreators(
   {
@@ -49,4 +61,7 @@ const mapDispatchToProps = (dispatch) => bindActionCreators(
   dispatch
 );
 
-export default connect(mapDispatchToProps)(CommonMessage);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CommonMessage);
