0451ad303315a49a23be1e5d20786221709efdfd
mseaward <m.seaward@pbsa.info>
2018-09-21 14:21:31 -0300

    Simplified the rendering process for the two, currently known, locations that the common message component is to render. - Currently displaying static content. - Does not have support for new dynamic messages being added yet. - Does not have support for dynamic height adjustment of the exchange and betslip panel which is to be derived from the number of messages that are currently rendered in the exchange location.

--- src/components/CommonMessage/CommonMessage.jsx
+++ src/components/CommonMessage/CommonMessage.jsx
@@ -7,23 +7,37 @@ import CommonMessageActions from '../../actions/CommonMessageActions';
 const compileMessage = (props) => {
   // Compile and render the exchange messages.
   const remove = (e) => {
+    // extract the id to pass to reducer so that we can remove the message from state.
     const msgParent = e.target.parentElement.parentElement.parentElement;
-    // remove the message
-    //msgParent.remove();
-    //extract the id to pass to reducer so that we can remove the message from state.
     const id = msgParent.id;
     
     props.hideMessage(id);
   };
 
-  const eM = props.exchangeMessages.map((pair, key) => (
-    <div className={ 'message-background ' + pair.get('messageType') } key={ key } id={ 'e'+key }>
+  let idPrefix, messageList;
+
+  if (props.location === 'exchange') {
+    idPrefix = 'e';
+    messageList = props.exchangeMessages;
+  }
+
+  if (props.location === 'betslip') {
+    idPrefix = 'b';
+    messageList = props.betslipMessages;
+  }
+
+  const messages = messageList.map((pair, key) => (
+    <div
+      className={ 'message-background ' + pair.get('messageType') }
+      key={ key }
+      id={ idPrefix + key }
+    >
       <div className='message-content'>
         <span>{pair.get('content') } <p onClick={ remove }>X</p></span>
       </div>
     </div>
   ));
-  return <div>{eM}</div>;
+  return <div>{messages}</div>;
 };
 
 class CommonMessage extends PureComponent {
