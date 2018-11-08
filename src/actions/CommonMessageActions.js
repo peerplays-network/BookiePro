ff8e2de9f6943eef5b0df19928140ee324c7e9f6
mseaward <m.seaward@pbsa.info>
2018-11-08 08:22:24 -0400

    Implement error messaging in the quickbetdrawer and the placed bets tab. - One or more bets exceeds balance - One or more bets is incomplete

--- src/actions/CommonMessageActions.js
+++ src/actions/CommonMessageActions.js
@@ -29,21 +29,25 @@ class CommonMessageActions {
    *
    *
    * @static
-   * @param {*} content - The message to be added.
-   * @param {*} messageType - 'info', 'success', 'warning', or 'error'
-   * @param {*} loc - 'exchange' or 'betslip'. Where the message is to appear.
+   * @param {string} content - The message to be added.
+   * @param {string} messageType - 'info', 'success', 'warning', or 'error'
+   * @param {string} loc - 'exchange' or 'betslip'. Where the message is to appear.
+   * @param {string} id - optional variable. If not supplied, one will be generated.
    * @returns
    * @memberof CommonMessageActions
    */
-  static newMessage(content, messageType, loc) {
+  static newMessage(content, messageType, loc, id) {
     return (dispatch, getState) => {
-      // Generate a unique id for the new message based on the existing number of messages.
-      let id = getState().get('commonMessage').get('messageCount') + 1;
-
-      if (loc === MessageLocation.EXCHANGE) {
-        id = 'e' + id;
-      } else {
-        id = 'b' + id;
+      // If an id has not been explicitely defined...
+      if (!id) {
+        // Generate a unique id for the new message based on the existing number of messages.
+        id = getState().get('commonMessage').get('messageCount') + 1;
+        
+        if (loc === MessageLocation.EXCHANGE) {
+          id = 'e' + id;
+        } else {
+          id = 'b' + id;
+        }
       }
 
       dispatch(CommonMessagePrivateActions.addMessage(content, messageType, loc, id));
