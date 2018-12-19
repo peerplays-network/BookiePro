8d4ca768db9fab6d818d8e2b08ab87c015be385f
mseaward <m.seaward@pbsa.info>
2018-12-19 15:57:22 -0400

    Modify indentation of server.js

--- web/server.js
+++ web/server.js
@@ -4,29 +4,31 @@ var isDevNet = (process.env.DEV_NET || false);
 var webpack = require('webpack');
 var ProgressPlugin = require('webpack/lib/ProgressPlugin');
 var WebpackDevServer = require('webpack-dev-server');
-var config = isDevNet
-    ? require('./conf/webpack-dev-net')
-    : require(env
-        ? './conf/webpack-dev-ugly'
-        : './conf/webpack-dev'
-    );
+var config = isDevNet ?
+  require('./conf/webpack-dev-net') :
+  require(env ?
+    './conf/webpack-dev-ugly' :
+    './conf/webpack-dev'
+  );
 
 var compiler = webpack(config);
 
 
 compiler.apply(new ProgressPlugin(function (percentage, msg) {
-    process.stdout.write((percentage * 100).toFixed(2) + '% ' + msg + '                 \033[0G');
+  process.stdout.write((percentage * 100).toFixed(2) + '% ' + msg + '                 \033[0G');
 }));
 new WebpackDevServer(compiler, {
-    publicPath: config.output.publicPath,
-    hot: true,
-    historyApiFallback: true,
-    quiet: false,
-    stats: {colors: true},
-    port: 8082
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  quiet: false,
+  stats: {
+    colors: true
+  },
+  port: 8082
 }).listen(8082, '0.0.0.0', function (err, result) {
-    if (err) {
-        console.log(err);
-    }
-    console.log('Listening at 0.0.0.0:8082');
+  if (err) {
+    console.log(err);
+  }
+  console.log('Listening at 0.0.0.0:8082');
 });
