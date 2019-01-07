e77ad9974b0501899ebf00640e926eb508702a3c
mseaward <m.seaward@pbsa.info>
2019-01-07 12:29:52 -0400

    Update server start (npm run start or npm start). - will auto open in browser properly 	- via opn package. - will detect if port is already being used, prompt for a new one, and apply a new one. Added a debug script command. - uses same server.js but with a debug flag.

--- web/server.js
+++ web/server.js
@@ -11,35 +11,88 @@ var config = isDevNet ?
     './conf/webpack-dev'
   );
 var chalk = require('chalk');
-var openBrowser = require('react-dev-utils/openBrowser');
+var opn = require('opn');
+var detect = require('detect-port');
+var clearConsole = require('react-dev-utils/clearConsole');
+var prompt = require('react-dev-utils/prompt');
+var getProcessForPort = require('react-dev-utils/getProcessForPort');
 var compiler = webpack(config);
+
+var isInteractive = process.stdout.isTTY;
 var DEFAULT_PORT = process.env.PORT || 8082;
 
-compiler.apply(new ProgressPlugin(function (percentage, msg) {
-  process.stdout.write(chalk.green(
-    (percentage * 100).toFixed(2) + '% ' + msg + '                 \033[0G'
-  ));
-}));
+function setupCompiler(host, port, protocol) {
+  compiler.apply(new ProgressPlugin(function (percentage, msg) {
+    process.stdout.write(chalk.green(
+      (percentage * 100).toFixed(2) + '% ' + msg + '                 \033[0G'
+    ));
+  }));
+}
 
-new WebpackDevServer(compiler, {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true,
-  // Switch assets, children, chunks to see verbose information in console.
-  stats: {
-    assets: false,
-    children: false,
-    colors: true,
-    chunks: false,
-    errors: true,
-    errorDetails: true,
-    warnings: true
-  },
-  port: DEFAULT_PORT
-}).listen(DEFAULT_PORT, '0.0.0.0', function (err, result) {
-  if (err) {
-    console.log(chalk.red(err));
+function runDevServer(host, port, protocol) {
+  var devServer = new WebpackDevServer(compiler, {
+    compress: true,
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+    // Switch assets, children, chunks to see verbose information in console.
+    stats: {
+      assets: false,
+      children: false,
+      colors: true,
+      chunks: false,
+      errors: true,
+      errorDetails: true,
+      warnings: true
+    },
+    // Enable HTTPS if the HTTPS environment variable is set to 'true'
+    https: protocol === 'https',
+    host: host
+  });
+
+  devServer.listen(port, (err, result) => {
+    if (err) {
+      console.log(chalk.red(err));
+    }
+    console.log(
+      chalk.cyan('Starting the development server... \n') +
+      chalk.yellow('Listening at ' + host + ':' + port + '/')
+    );
+    console.log();
+    opn(protocol + '://' + host + ':' + port + '/');
+  })
+}
+
+function run(port) {
+  var protocol = process.env.HTTPS === 'true' ? "https" : "http";
+  var host = process.env.HOST || 'localhost';
+  setupCompiler(host, port, protocol);
+  runDevServer(host, port, protocol);
+}
+
+// We attempt to use the default port but if it is busy, we offer the user to
+// run on a different port. `detect()` Promise resolves to the next free port.
+detect(DEFAULT_PORT).then(port => {
+  if (port === DEFAULT_PORT) {
+    run(port);
+    return;
   }
-  console.log(chalk.yellow('Listening at 0.0.0.0:8082'));
-  openBrowser('localhost:' + DEFAULT_PORT + '/');
+
+  if (isInteractive) {
+    clearConsole();
+    var existingProcess = getProcessForPort(DEFAULT_PORT);
+    var question =
+      chalk.yellow('Something is already running on port ' + DEFAULT_PORT + '.' +
+        ((existingProcess) ? ' Probably:\n  ' + existingProcess : '')) +
+      '\n\nWould you like to run the app on another port instead?';
+
+    prompt(question, true).then(shouldChangePort => {
+      if (shouldChangePort) {
+        console.log(chalk.green('Using port: ' + port + ' instead.'))
+        run(port);
+      }
+    });
+  } else {
+    console.log(chalk.red('Something is already running on port ' + DEFAULT_PORT + '.'));
+  }
 });
