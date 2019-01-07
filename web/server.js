97a9f752b8fb360373f88b71a91dc2dd46786aa3
mseaward <m.seaward@pbsa.info>
2019-01-07 11:12:44 -0400

    Update server output in console to only display errors.

--- web/server.js
+++ web/server.js
@@ -12,16 +12,26 @@ var compiler = webpack(config);
 compiler.apply(new ProgressPlugin(function (percentage, msg) {
     process.stdout.write((percentage * 100).toFixed(2) + '% ' + msg + '                 \033[0G');
 }));
+
 new WebpackDevServer(compiler, {
-    publicPath: config.output.publicPath,
-    hot: true,
-    historyApiFallback: true,
-    quiet: false,
-    stats: {colors: true},
-    port: 8082
-}).listen(8082, '0.0.0.0', function (err, result) {
-    if (err) {
-        console.log(err);
-    }
-    console.log('Listening at 0.0.0.0:8082');
+  publicPath: config.output.publicPath,
+  hot: true,
+  historyApiFallback: true,
+  // Switch assets, children, chunks to see verbose information in console.
+  stats: {
+    assets: false,
+    children: false,
+    colors: true,
+    chunks: false,
+    errors: true,
+    errorDetails: true,
+    warnings: true
+  },
+  port: DEFAULT_PORT
+}).listen(DEFAULT_PORT, '0.0.0.0', function (err, result) {
+  if (err) {
+    console.log(chalk.red(err));
+  }
+  console.log(chalk.yellow('Listening at 0.0.0.0:8082'));
+  openBrowser('localhost:' + DEFAULT_PORT + '/');
 });
