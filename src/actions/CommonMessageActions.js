baa8752a8896df8d350baf089a685f5034dcee97
Adrian <a.metzler@pbsa.info>
2019-01-31 15:44:08 -0400

    added ability to filter/whitelist redux actions

---
+++ src/actions/CommonMessageActions.js
@@ -0,0 +1,58 @@
+import {ActionTypes} from '../constants';
+import MessageLocation from '../constants/MessageLocation';
+
+class CommonMessagePrivateActions {
+  static removeMessage(id) {
+    return {
+      type: ActionTypes.COMMON_MSG_REMOVE_MESSAGE,
+      id
+    };
+  }
+  static addMessage(content, messageType, loc, id) {
+    return {
+      type: ActionTypes.COMMON_MSG_ADD_MSG,
+      content,
+      messageType,
+      loc,
+      id
+    };
+  }
+}
+
+class CommonMessageActions {
+  static clearMessage(id) {
+    return (dispatch) => {
+      dispatch(CommonMessagePrivateActions.removeMessage(id));
+    };
+  }
+  /**
+   *
+   *
+   * @static
+   * @param {string} content - The message to be added.
+   * @param {string} messageType - 'info', 'success', 'warning', or 'error'
+   * @param {string} loc - 'exchange' or 'betslip'. Where the message is to appear.
+   * @param {string} id - optional variable. If not supplied, one will be generated.
+   * @returns
+   * @memberof CommonMessageActions
+   */
+  static newMessage(content, messageType, loc, id) {
+    return (dispatch, getState) => {
+      // If an id has not been explicitely defined...
+      if (!id) {
+        // Generate a unique id for the new message based on the existing number of messages.
+        id = getState().get('commonMessage').get('messageCount') + 1;
+
+        if (loc === MessageLocation.EXCHANGE) {
+          id = 'e' + id;
+        } else {
+          id = 'b' + id;
+        }
+      }
+
+      dispatch(CommonMessagePrivateActions.addMessage(content, messageType, loc, id));
+    };
+  }
+}
+
+export default CommonMessageActions;
--- src/store/configureStore.dev.js
+++ src/store/configureStore.dev.js
@@ -1,17 +1,46 @@
 import {createStore, applyMiddleware, compose} from 'redux';
+import Config from '../../config/Config';
 import thunk from 'redux-thunk';
 // import createLogger from 'redux-logger';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {autoRehydrate} from 'redux-persist-immutable';
 import rootReducer from '../reducers';
 // import DevTools from 'containers/DevTools';
 import {hashHistory} from 'react-router';
 import {routerMiddleware} from 'react-router-redux';
+//import * as actionCreators from '../actions/CommonMessageActions';
 
 const middleware = routerMiddleware(hashHistory);
 
 export default function configureStore(preloadedState) {
+
+  let actionsWhitelist;
+
+  if (Config.commonMessageModule.disableActionsInRedux) {
+    actionsWhitelist = ['SEND_PAGE_UPDATE'];//add actions to whitelist in order to see them
+  }
+
+  // Configure enhancer for redux dev tools extensions (if available)
+  const composeEnhancers = composeWithDevTools({
+    features: {
+      dispatch: true
+    },
+    // Option for immutable
+    //serialize: {immutable: Immutable},
+    actionsWhitelist: actionsWhitelist,
+    //actionCreators: {actionCreators}
+  });
+
+  // Construct enhancer
+  const enhancer = composeEnhancers(
+    applyMiddleware(thunk, routerMiddleware(hashHistory)),
+    autoRehydrate()
+  );
+
   const store = createStore(
     rootReducer,
     preloadedState,
+    enhancer,
     compose(
       applyMiddleware(thunk/*, createLogger()*/, middleware),
       //DevTools.instrument(),
