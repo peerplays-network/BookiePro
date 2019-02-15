a3d5c58a0fcecf11a94ecb07ebec6ae1ec6449d8
Adrian <a.metzler@pbsa.info>
2019-02-15 12:54:59 -0400

    added back proposal, walletchangepassword, and tests

---
+++ src/__tests__/__tests__/components/Utility/FormattedAsset-test.jsx
@@ -0,0 +1,10 @@
+//unused
+import React from "react/addons";
+var TestUtils = React.addons.TestUtils;
+jest.dontMock("../../../app/components/Utility/FormattedAsset.jsx");
+
+describe("<FormattedAsset>", function() {
+    var FormattedAsset = require("../../../app/components/Utility/FormattedAsset.jsx");
+
+});
+
