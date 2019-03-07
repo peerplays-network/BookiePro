dd6c95aeb62042b59adbb80937f7761bb47d18db
Adrian <a.metzler@pbsa.info>
2019-03-07 10:35:48 -0400

    adding connection manager to config

--- config/Config.js
+++ config/Config.js
@@ -2,7 +2,7 @@ const _ = require('lodash');
 const {version} = require('../package.json');
 const getClientEnvironment = require('./env');
 const env = getClientEnvironment();
-
+const {Manager} = require('../../peerplaysjs-ws/');
 // Get the endpoints available.
 const blockchainUrls = env.raw.apiEndpoints;
 // Get the faucet endpoints available.
@@ -11,6 +11,21 @@ const faucetUrls = env.raw.faucetUrls;
 // Shuffle the available enpoints.
 const shuffledBlockchainUrls = _.shuffle(blockchainUrls);
 const shuffledFaucetUrls = _.shuffle(faucetUrls);
+
+let x = new Manager({
+  url: 'wss://api.ppytest.blckchnd.com',
+  urls: blockchainUrls
+});
+
+let t1 = new Date().getTime();
+x.checkConnections().then((response) => {
+  console.log(response);
+  let sorted = Object.keys(response).sort((a,b) => response[a]-response[b]);
+  console.log(new Date().getTime() - t1);
+
+  console.log(sorted);
+
+});
 
 const Config = {
   APP_VERSION: version,
