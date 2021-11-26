30c5017037d1505b1bc9be120cfb07e2f595694a
Evan Lynch <elynch@peerplays.com>
2021-11-26 08:45:17 -0500

    #36 update config for iron: - still need to find account keys for brodcasts and updates

--- config.js
+++ config.js
@@ -17,24 +17,22 @@ const config = {
     alice: {
       assetId: '1.3.1',
       apiEndpoints: [
-        // 'wss://api.eifos.org', // eifos-witness
-        // 'wss://api.ppy.us.altcap.io', // winner.winner.chicken.dinner
-        // 'wss://api.ppy.nuevax.com', // nuevax
-        // 'ws://18.184.122.253:8090', // phi-guy
-        // 'wss://peerplaysblockchain.net/mainnet/api', // houdini-witness
-        // 'wss://ppyseed.spacemx.tech', // spacecrypt-witness
-        // 'wss://api.ppy.alex-pu.info', // alex-pu
-        // 'wss://ip254.ip-91-121-48.eu', // melea-trust
-        // 'wss://api.ppy.blckchnd.com', // blckchnd
-        // 'wss://pma.blockveritas.co/ws', // taconator-witness-wallet
-        // 'wss://node.peerblock.trade:8090', // bitcoinsig
-        // 'wss://api2.ppy.blckchnd.com/ws', // royal-flush
-        // 'wss://ppyws.roelandp.nl/ws', // roelandp
-        'wss://irona.peerplays.download/api'
+        'wss://api.eifos.org', // eifos-witness
+        'wss://api.ppy.us.altcap.io', // winner.winner.chicken.dinner
+        'wss://api.ppy.nuevax.com', // nuevax
+        'ws://18.184.122.253:8090', // phi-guy
+        'wss://peerplaysblockchain.net/mainnet/api', // houdini-witness
+        'wss://ppyseed.spacemx.tech', // spacecrypt-witness
+        'wss://api.ppy.alex-pu.info', // alex-pu
+        'wss://ip254.ip-91-121-48.eu', // melea-trust
+        'wss://api.ppy.blckchnd.com', // blckchnd
+        'wss://pma.blockveritas.co/ws', // taconator-witness-wallet
+        'wss://node.peerblock.trade:8090', // bitcoinsig
+        'wss://api2.ppy.blckchnd.com/ws', // royal-flush
+        'wss://ppyws.roelandp.nl/ws', // roelandp
       ],
       faucetUrls: [
-        // 'https://btfun-faucet.bookie.download/faucet'
-        'http://irona-faucet.peerplays.download/faucet'
+        'https://btfun-faucet.bookie.download/faucet'
       ],
       prefix: 'PPY',
       accounts: {
@@ -52,19 +50,38 @@ const config = {
     beatrice: {
       assetId: '1.3.1',
       apiEndpoints: [
-        // 'wss://api-beatrice01.eifos.org',
-        // 'wss://testnet-ppyapi.spacemx.tech',
-        // 'wss://api-testnet.ppy.alex-pu.info',
-        // 'wss://api.ppy-beatrice.blckchnd.com',
-        // 'wss://pta.blockveritas.co/ws',
-        // 'wss://peerplaysblockchain.net/testnet/api',
-        // 'wss://bnode2.peerblock.trade',
-        // 'wss://test.phiguy.info/',
-        // 'wss://api.ppytest.nuevax.com/ws',
+        'wss://api-beatrice01.eifos.org',
+        'wss://testnet-ppyapi.spacemx.tech',
+        'wss://api-testnet.ppy.alex-pu.info',
+        'wss://api.ppy-beatrice.blckchnd.com',
+        'wss://pta.blockveritas.co/ws',
+        'wss://peerplaysblockchain.net/testnet/api',
+        'wss://bnode2.peerblock.trade',
+        'wss://test.phiguy.info/',
+        'wss://api.ppytest.nuevax.com/ws',
+      ],
+      faucetUrls: [
+        'https://beatrice-faucet.peerplays.download/faucet'
+      ],
+      prefix: 'TEST',
+
+      accounts: {
+        broadcasts: {
+          name: 'pbsa-broadcasts',
+          key: 'PPYTEST8H4L2UeaXRRAt5nVR4GSGFdt232711wyzTQnFRJeoJeLXXZT23'
+        },
+        updates: {
+          name: 'bookie-updates',
+          key: '5Kjqz8HwRBCW7ZtvhmM2NhAqaPpLQvBShKjVNcKdbm8gdXi5V3J'
+        }
+      }
+    },
+    irona: {
+      assetId: '1.3.1',
+      apiEndpoints: [
         'wss://irona.peerplays.download/api'
       ],
       faucetUrls: [
-        // 'https://beatrice-faucet.peerplays.download/faucet'
         'http://irona-faucet.peerplays.download/faucet'
       ],
       prefix: 'TEST',
@@ -74,7 +91,7 @@ const config = {
           name: 'pbsa-broadcasts',
           key: 'PPYTEST8H4L2UeaXRRAt5nVR4GSGFdt232711wyzTQnFRJeoJeLXXZT23'
         },
-      updates: {
+        updates: {
           name: 'bookie-updates',
           key: '5Kjqz8HwRBCW7ZtvhmM2NhAqaPpLQvBShKjVNcKdbm8gdXi5V3J'
         }
@@ -83,14 +100,11 @@ const config = {
   }
 };
 
-let current = 'alice'; // Set the current default.
+let current = 'irona'; // Set the current default.
+
 // console.log(`config.js === ${current}`);
 // Check to make sure a valid target is specified.
-if (
-  process.env &&
-  process.env.TARGET &&
-  config.environments[process.env.TARGET]
-) {
+if (process.env && process.env.TARGET && config.environments[process.env.TARGET]) {
   current = process.env.TARGET;
   console.log(`Environment defined as ${current}`);
 } else if (!process.env.TARGET) {
