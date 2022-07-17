bc13da93f60c92fa3c9a95a7154fae39f3150c63
bgerze <bgerze@peerplays.com>
2022-07-17 13:24:20 +0300

    Window installer automati script Automatic Scripting for windows
    	modified:   .gitignore
    	new file:   hybrid_bundle/SixPRO_INSTALLER_WIN/config/config.xml
    	new file:   hybrid_bundle/SixPRO_INSTALLER_WIN/config/config.xml.2
    	new file:   hybrid_bundle/SixPRO_INSTALLER_WIN/packages/com.peerplays.sixpro/meta/installscript.qs
    	new file:   hybrid_bundle/SixPRO_INSTALLER_WIN/packages/com.peerplays.sixpro/meta/package.xml
    	new file:   hybrid_bundle/SixPRO_INSTALLER_WIN/packages/com.peerplays.sixpro/meta/page.ui
    	new file:   hybrid_bundle/SixPRO_INSTALLER_WIN/sixpro_installer_win.bat

---
+++ hybrid_bundle/SixPRO_INSTALLER_WIN/sixpro_installer_win.bat
@@ -0,0 +1,6 @@
+SET QT_INSTALLER_FRAMEWORK_PATH=C:/Qt/Tools/QtInstallerFramework/4.4
+REM cd  SixPRO_INSTALLER_WIN
+
+%QT_INSTALLER_FRAMEWORK_PATH%/bin/binarycreator  -c  config/config.xml  -p packages/  SIXPRO_WEB_APP_installer.exe
+
+
